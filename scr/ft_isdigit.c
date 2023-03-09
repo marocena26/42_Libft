b8132c940d1793aef605618ecedde8a7165fef19
maarocen <maarocen@student.42madrid.com>
2023-03-09 17:58:48 +0100

    update

--- scr/ft_isdigit.c
+++ scr/ft_isdigit.c
@@ -6,8 +6,24 @@
 /*   By: maarocen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 16:49:45 by maarocen          #+#    #+#             */
-/*   Updated: 2023/03/09 16:49:47 by maarocen         ###   ########.fr       */
+/*   Updated: 2023/03/09 17:19:16 by maarocen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int	ft_isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
+// #include<stdio.h>
+// int	main(void)
+// {
+// 	int	c;
+
+// 	c = '2';
+// 	ft_isdigit(c);
+// 	printf("%d", ft_isdigit(c));
+// }
