d7d4334b3b2f77894423b7dfa95ebba5674e6df8
marocena26 <mariaelena.arocena@gmail.com>
2023-03-14 16:12:31 +0100

    add tester to functions

--- test/main_isdigit.c
+++ test/main_isdigit.c
@@ -1,22 +1,10 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_isdigit.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: maarocen <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/09 16:49:45 by maarocen          #+#    #+#             */
-/*   Updated: 2023/03/09 17:19:16 by maarocen         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
- #include<stdio.h>
- int	ft_isdigit(int c);
- int	main(void)
- {
- 	int	c;
-
- 	c = '2';
+#include<stdio.h>
+int	ft_isdigit(int c);
+int	main(void)
+{
+	int	c;
+	
+	c = '2';
 	ft_isdigit(c);
- 	printf("%d", ft_isdigit(c));
- }
+	printf("%d", ft_isdigit(c));
+}
