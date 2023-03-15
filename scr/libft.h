4adeb97ceeab74d00bf4461f0a5253eae2febf50
marocena26 <mariaelena.arocena@gmail.com>
2023-03-15 15:10:55 +0100

    start create ft with main

--- scr/libft.h
+++ scr/libft.h
@@ -1,25 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maarocen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/15 14:36:29 by maarocen          #+#    #+#             */
-/*   Updated: 2023/03/15 15:05:02 by maarocen         ###   ########.fr       */
+/*   Created: 2023/03/07 15:41:13 by maarocen          #+#    #+#             */
+/*   Updated: 2023/03/09 13:13:32 by maarocen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#ifndef LIBFT_H
+#define LIBFT_H
 
-size_t ft_strlen(const char *s)
-{
-    size_t	length;
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-	length = 0;
-	while (s[length] != '\0')
-	{
-		length++;
-	}
-	return (length);
-}
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+size_t ft_strlen(const char *s);
+
+#endif
